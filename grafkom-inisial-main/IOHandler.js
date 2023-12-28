eb0c6c267cd3efbb275a22d3c5a7128b63e8bbf6
AFarimza <114476583+AFarimza@users.noreply.github.com>
2023-12-28 21:53:46 +0700

    Add files via upload

---
+++ grafkom-inisial-main/IOHandler.js
@@ -0,0 +1,90 @@
+var space = false;
+var w = false;
+var a = false;
+var s = false;
+var d = false;
+var left = false;
+var right = false;
+function onMouseClick(event){
+    w = !w;
+    a = !a;
+    left = !left;
+    
+}
+
+document.addEventListener('click', onMouseClick, false);
+
+function onKeydown(event){
+    if(event.keyCode == 32){
+        space = true;
+    }
+
+    if(event.keyCode == 87){
+        w = true;
+    }
+
+    if(event.keyCode == 65){
+        a= true;
+    }
+
+    if(event.keyCode == 83){
+        s= true;
+    }
+
+    if(event.keyCode == 68){
+        d= true;
+    }
+
+    if(event.keyCode == 37){
+        left= true;
+    }
+
+    if(event.keyCode == 39){
+        right = true;
+    }
+}
+
+function onKeyup(event){
+    if(event.keyCode == 32){
+        space = false;
+    }
+
+    if(event.keyCode == 87){
+        w = false;
+    }
+
+    if(event.keyCode == 65){
+        a = false;
+    }
+
+    if(event.keyCode == 83){
+        s = false;
+    }
+
+    if(event.keyCode ==68){
+       d = false;
+    }
+
+    if(event.keyCode == 37){
+        left= false;
+    }
+
+    if(event.keyCode == 39){
+        right = false;
+    }
+}
+
+// function onKeydown(event){
+//     if(event.keyCode == 32){
+//         freeze = true;
+//     }
+// }
+
+// function onKeyup(event){
+//     if(event.keyCode == 32){
+//         freeze = false;
+//     }
+// }
+
+document.addEventListener('keydown', onKeydown, true);
+document.addEventListener('keyup', onKeyup, true);
